1ae9717bb63fc622a4437eb595cba19b36ae3453
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2025-07-12 13:12:36 +0700

    feat: enhance deposits and orders schema with index for deposit_id and order_id

--- packages/db/src/schema/deposits.ts
+++ packages/db/src/schema/deposits.ts
@@ -7,25 +7,32 @@ import { timestamp } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { paymentMethods } from "./payments";
 import { DepositStatus, depositStatusEnum } from "./pg-enums";
+import { index } from "drizzle-orm/pg-core";
 
-export const deposits = pgTable("deposits", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  deposit_id: varchar("deposit_id", { length: 100 }).notNull().unique(),
-  user_id: uuid("user_id")
-    .references(() => users.id)
-    .notNull(),
-  payment_method_id: uuid("payment_method_id")
-    .references(() => users.id)
-    .notNull(),
-  amount_pay: integer("amount_pay").notNull(),
-  amount_received: integer("amount_received").notNull(),
-  amount_fee: integer("amount_fee").notNull().default(0),
-  status: depositStatusEnum("status").notNull().default(DepositStatus.PENDING),
-  created_at: timestamp("created_at", { withTimezone: true }).defaultNow(),
-  updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-    () => new Date()
-  ),
-});
+export const deposits = pgTable(
+  "deposits",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    deposit_id: varchar("deposit_id", { length: 100 }).notNull().unique(),
+    user_id: uuid("user_id")
+      .references(() => users.id)
+      .notNull(),
+    payment_method_id: uuid("payment_method_id")
+      .references(() => users.id)
+      .notNull(),
+    amount_pay: integer("amount_pay").notNull(),
+    amount_received: integer("amount_received").notNull(),
+    amount_fee: integer("amount_fee").notNull().default(0),
+    status: depositStatusEnum("status")
+      .notNull()
+      .default(DepositStatus.PENDING),
+    created_at: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => [index("deposits_deposit_id_index").on(table.deposit_id)],
+);
 
 export const depositRelations = relations(deposits, ({ one }) => ({
   payment_method: one(paymentMethods, {
--- packages/db/src/schema/orders.ts
+++ packages/db/src/schema/orders.ts
@@ -15,47 +15,52 @@ import {
   RefundStatus,
   refundStatusEnum,
 } from "./pg-enums";
+import { index } from "drizzle-orm/pg-core";
 
-export const orders = pgTable("orders", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  order_id: uuid("order_id").notNull().defaultRandom(),
+export const orders = pgTable(
+  "orders",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    order_id: uuid("order_id").notNull().defaultRandom(),
 
-  user_id: uuid("user_id")
-    .notNull()
-    .references(() => users.id),
-  offer_on_order_id: uuid("offer_on_order_id").references(
-    () => offerOnOrders.id
-  ),
-  product_snapshot_id: uuid("product_snapshot_id")
-    .notNull()
-    .references(() => productSnapshots.id),
-  payment_snapshot_id: uuid("payment_snapshot_id")
-    .notNull()
-    .references(() => productSnapshots.id),
+    user_id: uuid("user_id")
+      .notNull()
+      .references(() => users.id),
+    offer_on_order_id: uuid("offer_on_order_id").references(
+      () => offerOnOrders.id,
+    ),
+    product_snapshot_id: uuid("product_snapshot_id")
+      .notNull()
+      .references(() => productSnapshots.id),
+    payment_snapshot_id: uuid("payment_snapshot_id")
+      .notNull()
+      .references(() => productSnapshots.id),
 
-  total_price: integer("total_price").notNull(),
-  profit: integer("profit").notNull().default(0),
-  cost_price: integer("cost_price").notNull().default(0),
-  discount_price: integer("discount_price").notNull().default(0),
+    total_price: integer("total_price").notNull(),
+    profit: integer("profit").notNull().default(0),
+    cost_price: integer("cost_price").notNull().default(0),
+    discount_price: integer("discount_price").notNull().default(0),
 
-  sn_number: varchar("sn_number", { length: 150 }).notNull(),
-  notes: varchar("notes"),
+    sn_number: varchar("sn_number", { length: 150 }).notNull(),
+    notes: varchar("notes"),
 
-  payment_status: paymentStatusEnum("payment_status")
-    .notNull()
-    .default(PaymentStatus.PENDING),
-  order_status: orderStatusEnum("order_status")
-    .notNull()
-    .default(OrderStatus.NONE),
-  refund_status: refundStatusEnum("refund_status")
-    .notNull()
-    .default(RefundStatus.NONE),
+    payment_status: paymentStatusEnum("payment_status")
+      .notNull()
+      .default(PaymentStatus.PENDING),
+    order_status: orderStatusEnum("order_status")
+      .notNull()
+      .default(OrderStatus.NONE),
+    refund_status: refundStatusEnum("refund_status")
+      .notNull()
+      .default(RefundStatus.NONE),
 
-  created_at: timestamp("created_at", { withTimezone: true }).defaultNow(),
-  updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-    () => new Date()
-  ),
-});
+    created_at: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => [index("orders_order_id_index").on(table.order_id)],
+);
 
 export const orderRelations = relations(orders, ({ one }) => ({
   user: one(users, {
